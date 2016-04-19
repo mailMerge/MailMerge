67d358fdf3b931017aa4d7205582171e542221fe
bjmendez <bjmendez@buffalo.edu>
2016-04-19 13:13:12 -0400

    Update MailMergeAddressmethod.py

--- MailMergeAddressmethod.py
+++ MailMergeAddressmethod.py
@@ -20,7 +20,6 @@ testdf = pd.read_excel('C:/Users/Brandon/Documents/School/Mail Merge/test.xlsx')
 
 
 testdf = testdf.drop_duplicates(subset=['Address 1', 'Address 2', 'City', 'State', 'Zipcode'])
-buffalobuildings = ['Aliero Center','Alumni Arena', 'Baird Hall', 'Baldy Hall', 'Baird Research Park', 'Beane Center', 'Bell Hall', 'Bissell Hall', 'Bonner Hall','Capen Hall', 'Center for the Arts', 'Clemens Hall', 'Cooke Hall', ' Crofts Hall','Davis Hall','Fronczak Hall','Furnas Hall','Hochstetter Hall', 'Jarvis Hall', 'Ketter Hall', 'Mathematics Building', 'Millard Fillmore', 'Natural Sciences Complex', 'Norton Hall', 'Brian Hall', ' Park Hall', 'Slee Hall', 'Student Union', 'Talbert Hall','Beck Hall','Biomedical Education Building', 'Biomedical Research Building', 'Cary Hall', 'Crosby Hall', 'Foster Hall', 'Harriman Hall', 'Howe Building', 'Kapoor hall', 'Kimball Tower', 'Parker Hall', 'Sherman Annex', 'Squire Hall', 'Wende Hall']
 
 
 def addresscheck(df):
@@ -32,11 +31,11 @@ def addresscheck(df):
             wrongaddress.loc[len(wrongaddress)] = df.iloc[index]
 
         if addresscheck[1] != 'Ambiguous':
-            if addressstring.find('14261') == -1 and addressstring.find('14260') == -1 and addressstring.find('14214') == -1:
+            if addressstring.find('14261') == -1 and addressstring.find('14260') == -1 and addressstring.find('Clement') == -1 and addressstring.find('Goodyear') == -1:
                 usadd.loc[len(usadd)]= df.iloc[index]
 
         else:
-            if addressstring.find('14261') == -1 and addressstring.find('14260') == -1 and addressstring.find('14214') == -1:
+            if addressstring.find('14261') == -1 and addressstring.find('14260') == -1 and addressstring.find('Clement') == -1 and addressstring.find('Goodyear') == -1:
                 wrongaddress.loc[len(wrongaddress)] = df.iloc[index]
 
 
